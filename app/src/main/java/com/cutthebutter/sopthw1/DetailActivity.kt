19a50105ad61a2c1124bb5c2b1d4a3ca81c2d789
sem <uhoo1216@gmail.com>
2020-11-19 21:36:28 +0900

    과제 1, 2 모두 완료

--- app/src/main/java/com/cutthebutter/sopthw1/DetailActivity.kt
+++ app/src/main/java/com/cutthebutter/sopthw1/DetailActivity.kt
@@ -3,9 +3,9 @@ package com.cutthebutter.sopthw1
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class ProfileActivity : AppCompatActivity() {
+class DetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_profile)
+        setContentView(R.layout.activity_detail)
     }
 }
